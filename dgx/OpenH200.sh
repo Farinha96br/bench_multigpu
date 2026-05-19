c2520853b06a24f05eb399ea9334811784e0bf25
farinha96br <farinha96br@gmail.com>
2026-05-19 16:33:44 -0300

    removed the gpu configs, now passed via submitter

--- dgx/OpenH200.sh
+++ dgx/OpenH200.sh
@@ -3,12 +3,13 @@
 #SBATCH --time=0-12:00:00
 #SBATCH --partition=large
 #SBATCH --job-name=open
-#SBATCH --output=dgx_h200/open.txt
-#SBATCH --error=dgx_h200/open.txt
 #SBATCH --cpus-per-task=4
-#SBATCH --ntasks=4
-#SBATCH --gpus=4
-#SBATCH --mem=100G
+#SBATCH --mem=50G
+
+if [ -z "$SLURM_GPUS_ON_NODE" ] || [ "$SLURM_GPUS_ON_NODE" -eq 0 ]; then
+    echo "Error: SLURM_GPUS_ON_NODE is not set or is 0" >&2
+    exit 1
+fi
 
 module load apptainer 
 
@@ -24,22 +25,17 @@ devito_lang=openacc
 devito_arch=nvc
 devito_platform=nvidiaX
 
-space_orders
-
 
 domain_sizes=(256 512 1024)
 space_orders=(2 4 8 2 4 8)
 #            4k   4k    4k    400 400  400
 final_times=(11425 10349 9680 1138 1032 965) 
-gpunum=(1 2 4)
-
 repeats=5
 
 #SO=8:   9680   965
 #SO=4:  10349   1032
 #SO=2:  11425   1138
 
-bash clone_and_fix.sh
 
 for space_order_idx in "${!space_orders[@]}"
     do
@@ -48,45 +44,42 @@ for space_order_idx in "${!space_orders[@]}"
     for domain_size in "${domain_sizes[@]}"
     do
         DOMAIN_SIZE=$domain_size
-            for i in "${gpunum[@]}"
-            do
-                if [ $i -eq 1 ]
-                then
-                    DEVITO_MPI=0
-                else
-                    DEVITO_MPI=1
-                fi
-                GPU_NUM=$i
-                # now the repetitions
-                for j in $(seq 1 $repeats)
-                do
-                    echo "= START ="
-                    echo "run-number: $j"
-                    echo "domain-size: $DOMAIN_SIZE"
-                    echo "space-order: $SPACE_ORDER"
-                    echo "final-time: $FINAL_TIME" 
-                    echo "operator: $OPERATOR"
-                    echo "devito-version: open"
-                    echo "gpu-num: $GPU_NUM"
-                    echo "mpi: $DEVITO_MPI"
+        if [ $SLURM_GPUS_ON_NODE -eq 1 ]
+        then
+            DEVITO_MPI=0
+        else
+            DEVITO_MPI=1
+        fi
+        # now the repetitions
+        for j in $(seq 1 $repeats)
+        do
+            echo "= START ="
+            echo "run-number: $j"
+            echo "domain-size: $DOMAIN_SIZE"
+            echo "space-order: $SPACE_ORDER"
+            echo "final-time: $FINAL_TIME" 
+            echo "operator: $OPERATOR"
+            echo "devito-version: open"
+            echo "gpu-num: $SLURM_GPUS_ON_NODE"
+            echo "mpi: $DEVITO_MPI"
 
-                    apptainer exec --nv ${IMAGE_PATH} bash -c \
-                    "export DEVITO_LANGUAGE=openacc && \
-                    export DEVITO_PLATFORM=nvidiaX && \
-                    export DEVITO_ARCH=nvc && \
-                    source /venv/bin/activate && \
-                    export PYTHONPATH=devito/ && \
-                    export DEVITO_MPI=$DEVITO_MPI && \
-                    mpirun -np $GPU_NUM python3 $BENCHMARK_SCRIPT run \
-                    -P acoustic \
-                    -d $DOMAIN_SIZE $DOMAIN_SIZE $DOMAIN_SIZE \
-                    -so $SPACE_ORDER \
-                    -s 20.0 20.0 20.0  \
-                    --nbl 0 \
-                    --tn $FINAL_TIME"
+            apptainer exec --nv ${IMAGE_PATH} bash -c \
+            "export DEVITO_LANGUAGE=openacc && \
+            export DEVITO_PLATFORM=nvidiaX && \
+            export DEVITO_ARCH=nvc && \
+            source /venv/bin/activate && \
+            export PYTHONPATH=devito/ && \
+            export DEVITO_MPI=$DEVITO_MPI && \
+            mpirun -np $SLURM_GPUS_ON_NODE python3 $BENCHMARK_SCRIPT run \
+            -P acoustic \
+            -d $DOMAIN_SIZE $DOMAIN_SIZE $DOMAIN_SIZE \
+            -so $SPACE_ORDER \
+            -s 20.0 20.0 20.0  \
+            --nbl 0 \
+            --tn $FINAL_TIME"
 
-                    echo "= END ="
-                done
+            echo "= END ="
+                
             done
         done
     done
