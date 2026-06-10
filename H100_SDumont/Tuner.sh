371b3b16ff8d27e4ff388bf873b55ee7250454b2
farinha96br <farinha96br@gmail.com>
2026-06-10 16:14:03 -0300

    Tuning results for H100

---
+++ H100_SDumont/Tuner.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#SBATCH --begin=now
+#SBATCH --time=0-12:00:00
+#SBATCH --partition=lncc-h100
+#SBATCH --job-name=tuner
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=50G
+
+if [ -n "$SLURM_GPUS_ON_NODE" ]; then
+    GPUCOUNT=$SLURM_GPUS_ON_NODE
+else
+    GPUCOUNT=$SLURM_NTASKS
+fi
+
+if [ -z "$GPUCOUNT" ] || [ "$GPUCOUNT" -eq 0 ]; then
+    echo "Error: GPUCOUNT is not set or is 0" >&2
+    exit 1
+fi
+
+export IMAGE_PATH=/prj/cadase/andre.bosio/devitopro_update20260605.sif
+export APPTAINER_TMPDIR=/prj/cadase/andre.bosio/apptainertmpdir
+export APPTAINER_CACHEDIR=/prj/cadase/andre.bosio/apptainercachedir
+mkdir -p $APPTAINER_TMPDIR $APPTAINER_CACHEDIR
+
+export BENCHMARK_SCRIPT=minimal_acoustic.py
+
+devito_lang=cuda
+devito_arch=cuda
+
+domain_sizes=(1024 512 256)
+space_orders=(2 4 8)
+
+
+# for every space order
+for space_order_idx in "${!space_orders[@]}"
+    do
+        SPACE_ORDER=${space_orders[$space_order_idx]}
+    # for every domain size
+    for domain_size in "${domain_sizes[@]}"
+    do
+        DOMAIN_SIZE=$domain_size
+        # adjust for the issue with large domains
+        if [ $DOMAIN_SIZE -eq 1024 ]
+        then
+            FIXED_OPT="('fixed', {'index-mode': 'int64'})"
+        else
+            FIXED_OPT=" "
+        # organize the execution command based on the number of GPUs
+        fi
+        if [ "$GPUCOUNT" -eq 1 ]
+        then
+            DEVITO_MPI=0
+            EXEC_CMD="python3 -m devitotuner "
+        else
+            DEVITO_MPI="diag2"
+            EXEC_CMD="mpirun -np $GPUCOUNT python3 -m devitotuner "
+        fi
+    # now the repetitions
+        echo "= START ="
+        echo "TUNING WITH"
+        echo "devito-version: pro"
+
+        echo "gpu-num: $GPUCOUNT"
+        echo "domain-size: $DOMAIN_SIZE"
+        echo "space-order: $SPACE_ORDER"
+        echo "mpi: $DEVITO_MPI"
+
+        singularity exec --nv ${IMAGE_PATH} bash -c \
+        "export DEVITO_LANGUAGE=$devito_lang && \
+        export DEVITO_PLATFORM=nvidiaX && \
+        export DEVITO_ARCH=$devito_arch && \
+        export DEVITO_MPI=$DEVITO_MPI && \
+        export DEVITO_LOGGING=DEBUG && \
+        export DEVITO_TUNER_VERBOSE=0 && \
+        $EXEC_CMD Simple H100_SDumont/H100_so_${SPACE_ORDER}_d_${DOMAIN_SIZE}_gpu_${GPUCOUNT}.json \"$FIXED_OPT\" $BENCHMARK_SCRIPT \
+        -d $DOMAIN_SIZE $DOMAIN_SIZE $DOMAIN_SIZE \
+        -so $SPACE_ORDER \
+        -tn 100 \
+        -ngpus $GPUCOUNT"
+        echo "= END ="
+    done
+done
